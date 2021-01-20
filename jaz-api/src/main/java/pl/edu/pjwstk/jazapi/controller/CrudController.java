40603722051a92b204f427032416a2d7f4077112
Jan Sitarski <jansitarski00@gmail.com>
2021-01-20 13:11:20 +0100

    JWT

--- jaz-api/src/main/java/pl/edu/pjwstk/jazapi/controller/CrudController.java
+++ jaz-api/src/main/java/pl/edu/pjwstk/jazapi/controller/CrudController.java
@@ -1,19 +1,18 @@
 package pl.edu.pjwstk.jazapi.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.edu.pjwstk.jazapi.service.CrudService;
 import pl.edu.pjwstk.jazapi.service.DbEntity;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public abstract class CrudController<T extends DbEntity> {
-    @Autowired
     private final CrudService<T> service;
 
     public CrudController(CrudService<T> service) {
@@ -23,12 +22,18 @@ public abstract class CrudController<T extends DbEntity> {
     @GetMapping()
     public ResponseEntity<List<Map<String, Object>>> getAll(@RequestParam(defaultValue = "4") int size,
                                                             @RequestParam(defaultValue = "0") int page,
-                                                            @RequestParam(defaultValue = "id") String sort) {
+                                                            @RequestParam(defaultValue = "asc,id") String sort) {
         try {
-            List<T> all = service.getAll(page, size, sort);
+            List<T> all = service.getAll(size, page, sort);
             List<Map<String, Object>> payload = all.stream()
                     .map(obj -> transformToDTO().apply(obj))
                     .collect(Collectors.toList());
+
+            Map<String, Object> pagingInfo = new LinkedHashMap<>();
+            pagingInfo.put("pageSize", size);
+            pagingInfo.put("page", page);
+            pagingInfo.put("sort", sort);
+            payload.add(pagingInfo);
 
             return new ResponseEntity<>(payload, HttpStatus.OK);
         } catch (Exception e) {
